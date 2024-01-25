fa84bafd88da7195f863d179f5a58938bcd48f1d
demenciel <alexcouture5@hotmail.com>
2024-01-24 20:15:42 -0500

    video

--- src/sections/OurServices.jsx
+++ src/sections/OurServices.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Navigation, Pagination, A11y, EffectFade } from "swiper/modules";
 import "swiper/css"; // basic Swiper styles
 import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 
+import "../../public/styles.css";
 // photos
 import microblading from "../assets/Photos site genny/Microblading /20231126_112108.png";
 import freckles from "../assets/Photos site genny/Freckles/2e70c3bd-847c-4a03-b258-1fb52af2202d.png";
@@ -13,9 +15,12 @@ import ombrebrow from "../assets/Photos site genny/Ombrebrow-nanobrow/400235384_
 import micropigmentation from "../assets/Photos site genny/Micropigmentation capillaire /received_2082407108783656.jpeg";
 //videos
 import microbladingombrebrowvideo from "../assets/Photos site genny/video/microblading-ombrebrow.mp4";
+import micropigmentationvideo from "../assets/Photos site genny/video/micropigmentation.mp4";
+import frecklesvideo from "../assets/Photos site genny/video/frecklesvideo.mp4";
 import lipblushvideo from "../assets/Photos site genny/video/lipblush.mp4";
 import eyelinerintralashvideo from "../assets/Photos site genny/video/eyeliner.mp4";
 import VideoModal from "../components/Video";
+import { Card } from "@mui/material";
 
 // Mock data for the carousel cards
 const carouselData = [
@@ -27,7 +32,7 @@ const carouselData = [
   {
     title: "Freckles",
     imageUrl: freckles,
-    video: "",
+    video: frecklesvideo,
   },
   {
     title: "Eyeliner/Intralash",
@@ -47,7 +52,7 @@ const carouselData = [
   {
     title: "Micropigmentation capillaire",
     imageUrl: micropigmentation,
-    video: "",
+    video: micropigmentationvideo,
   },
 ];
 
@@ -67,26 +72,35 @@ const CarouselCard = ({ title, imageUrl, video }) => {
 
   return (
     <>
-      <div className="w-[300px] md:w-3/4 group flex cursor-pointer flex-col items-center justify-center bg-background p-4 shadow-md rounded-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl tilt">
-        <img
-          src={imageUrl}
-          alt={title}
-          className="w-[272px] h-[380px] object-cover rounded-t-lg"
+      <Card
+        sx={{ maxWidth: 345, backgroundColor: "#242424" }}
+        className="cursor-pointer "
+      >
+        <div className="w-full group flex  flex-col items-center justify-center bg-background p-4 shadow-md rounded-lg transition duration-300 ease-in-out transform hover:shadow-2xl">
+          <img
+            src={imageUrl}
+            alt={title}
+            className="w-[272px] h-[380px] object-cover rounded-t-lg"
+          />
+          <div className="absolute bottom-0 left-0 w-full h-2/3 bg-gradient-to-t from-black to-transparent transition duration-300 ease-in-out group-hover:opacity-0"></div>
+          <h3 className="mt-4 text-lg font-bold py-2 absolute bottom-20 text-cta">
+            {title}
+          </h3>
+          <button
+            onClick={() => {
+              setOpenModal(true);
+            }}
+            className="mt-2 bg-cta text-background py-2 px-4 rounded-full absolute bottom-8 group-hover:drop-shadow-lg"
+          >
+            Recommandations
+          </button>
+        </div>
+        <VideoModal
+          open={openModal}
+          setOpenModal={setOpenModal}
+          video={video}
         />
-        <div className="absolute bottom-0 left-0 w-full h-2/3 bg-gradient-to-t from-black to-transparent transition duration-300 ease-in-out group-hover:opacity-0"></div>
-        <h3 className="mt-4 text-lg font-bold py-2 absolute bottom-20 ">
-          {title}
-        </h3>
-        <button
-          onClick={() => {
-            setOpenModal(true);
-          }}
-          className="mt-2 bg-cta text-background py-2 px-4 rounded-full absolute bottom-8 group-hover:drop-shadow-lg"
-        >
-          Recommandations
-        </button>
-      </div>
-      <VideoModal open={openModal} setOpenModal={setOpenModal} video={video} />
+      </Card>
     </>
   );
 };
@@ -94,25 +108,24 @@ const CarouselCard = ({ title, imageUrl, video }) => {
 const OurServices = () => {
   return (
     <div id="ourservices" className="w-full bg-primary relative">
-      <div className="pb-16 pt-10">
+      <div className="pb-16 pt-10 h-full">
         <div className="w-full h-1/4 pb-8">
           <h1 className="text-center font-bold text-2xl md:text-3xl lg:text-4xl py-8 text-background">
             Nos services
           </h1>
         </div>
-        <div className="w-full h-full sm:px-16 md:px-32">
+        <div className="w-full sm:px-16 md:px-32">
           <Swiper
-            modules={[Navigation, Pagination, A11y]}
+            modules={[Navigation, Pagination, A11y, EffectFade]}
             spaceBetween={40} // Add space between slides
             slidesPerView={1}
             centeredSlides={true} // Center the active slide
             loop={true} // Enable looping
             speed={500} // Transition speed
-            autoHeight={true} // Adjust height to the active slide
+            autoHeight={true}
             navigation={{
-              nextEl: ".image-swiper-button-next",
-              prevEl: ".image-swiper-button-prev",
-              disabledClass: "swiper-button-disabled",
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
             }}
             breakpoints={{
               768: {
@@ -122,15 +135,12 @@ const OurServices = () => {
                 slidesPerView: 3,
                 spaceBetween: 20,
               },
+              1500: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+              },
             }}
-            effect={"coverflow"}
-            coverflowEffect={{
-              rotate: 50, // Slide rotate in degrees
-              stretch: 0, // Stretch space between slides (in px)
-              depth: 100, // Depth offset in px (slides translate in Z axis)
-              modifier: 1, // Effect multiplier
-              slideShadows: true, // Enables slides shadows
-            }}
+            effect={"effect-fade"}
             freeMode={true} // Enable free mode
           >
             {carouselData.map((card, index) => (
